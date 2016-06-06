106ed391b14144ab1975f5b2ce612bcb82217ddb
sarahlobser <sarah.lobser@gmail.com>
2016-06-06 10:43:35 -0600

    Added cart cookie. Added routes in product routes to add product to cart and empty cart. Added two methods in productCtrl to add to cart and empty cart.

--- app_api/controllers/productCtrl.js
+++ app_api/controllers/productCtrl.js
@@ -62,3 +62,33 @@ module.exports.update = function(req,res){
             res.sendStatus(202);
         });
 };
+
+module.exports.addToCart = function(req, res) {
+    var product = models.Product.findById(req.params.id)
+        .then(function(product) {
+            var cart = [];
+            if(req.signedCookies.cart) {
+                cart = req.signedCookies.cart;
+                cart.push(product);
+                res.cookie('cart', cart, {signed:true});
+                res.render('products', {cartitems:cart});
+            } else {
+                cart.push(product);
+                res.cookie('cart', cart, {signed:true});
+                res.render('products', {cartitems:cart});
+            }
+        });
+};
+
+module.exports.emptyCart = function(req, res) {
+    console.log("in empty cart");
+    if(req.signedCookies.cart) {
+        cart = req.signedCookies.cart;
+        console.log("cart has " + cart.length + " it");
+        while(cart.length > 0) {
+            cart.pop();
+        }
+    }
+    res.clearCookie('cart');
+    res.send('Cart has been emptied...sucka');
+};
--- app_api/routes/productRoutes.js
+++ app_api/routes/productRoutes.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 var productCtrl = require('../controllers/productCtrl');
 
+router.get('/cart/:id', productCtrl.addToCart);
+router.get('/cart', productCtrl.emptyCart);
 router.get('/', productCtrl.index);
 router.get('/:id', productCtrl.show);
 router.post('/', productCtrl.create);
 router.delete('/:id', productCtrl.destroy);
 router.put('/:id', productCtrl.update);
+
 
 module.exports = router;
