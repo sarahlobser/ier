f98cd2cbb4e359b84e8875b9d538742660d204d5
David Avila <usafdvd@users.noreply.github.com>
2016-06-06 16:26:17 -0600

    made minor changes

--- app_server/controllers/ctrl.js
+++ app_server/controllers/ctrl.js
@@ -3,55 +3,67 @@ var models = require('../../app_api/models');
 
 var request = require('request');
 
-module.exports.getAll = function(req, res) {
-    request.get('http://localhost:3000/api/products/', function(error, response, body){
-        if(!error) {
-            res.render('products', {products: JSON.parse(body)});
+module.exports.getAll = function (req, res) {
+    request.get('http://localhost:3000/api/products/', function (error, response, body) {
+        if (!error) {
+            res.render('products', {
+                products: JSON.parse(body)
+            });
         } else {
             res.sendStatus(500);
         }
     })
 };
 
-module.exports.show = function(req, res) {
-    request.get('http://localhost:3000/api/products/' + req.params.id, function(error, response, body){
-        if(!error) {
-            res.render('products', {product: JSON.parse(body)});
+module.exports.show = function (req, res) {
+    request.get('http://localhost:3000/api/products/' + req.params.id, function (error, response, body) {
+        if (!error) {
+            res.render('products', {
+                product: JSON.parse(body)
+            });
         } else {
             res.sendStatus(500);
         }
     })
 }
 
-module.exports.addToCart = function(req, res) {
+module.exports.addToCart = function (req, res) {
     var product = models.Product.findById(req.params.id)
-        .then(function(product) {
+        .then(function (product) {
+            console.log("HELLO")
             var cart = [];
-            if(req.signedCookies.cart) {
+            if (req.signedCookies.cart) {
                 cart = req.signedCookies.cart;
                 cart.push(product);
-                res.cookie('cart', cart, {signed:true});
-                res.render('products', {cartitems:cart});
+                res.cookie('cart', cart, {
+                    signed: true
+                });
+                res.render('products', {
+                    cartitems: cart
+                });
             } else {
                 cart.push(product);
-                res.cookie('cart', cart, {signed:true});
-                res.render('products', {cartitems:cart});
+                res.cookie('cart', cart, {
+                    signed: true
+                });
+                res.render('products', {
+                    cartitems: cart
+                });
             }
         });
 };
 
-module.exports.emptyCart = function(req, res) {
+module.exports.emptyCart = function (req, res) {
     console.log("in empty cart");
-    if(req.signedCookies.cart) {
+    if (req.signedCookies.cart) {
         //cart = [];
         var cart = req.signedCookies.cart;
         console.log("cart has " + cart.length + " it");
-        while(cart.length > 0) {
+        while (cart.length > 0) {
             cart.pop();
         }
-        
+
     }
     res.clearCookie('cart');
     res.send('Cart has been emptied...sucka');
 };
-
