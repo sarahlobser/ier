367cc4c21c38ab09c2c76a3d67721cdc6436c198
David Avila <usafdvd@users.noreply.github.com>
2016-06-06 08:47:29 -0600

    Commented out changes to employee authentication. Not sure if we need this

--- app_server/controllers/loginCtrl.js
+++ app_server/controllers/loginCtrl.js
@@ -3,45 +3,59 @@ var models = require('../../app_api/models');
 var bcrypt = require('bcrypt');
 const saltRounds = 13;
 
-module.exports.home = function(req,res) {
-	res.render('index', {user : req.user});
+module.exports.home = function (req, res) {
+    res.render('index', {
+        user: req.user
+    });
 };
 
-module.exports.login = function(req,res) {
-	res.render('login')
+module.exports.login = function (req, res) {
+    res.render('login')
 };
 
-module.exports.authenticate = function(req,res) {
-	console.log(req.body);
-	passportConfig.authenticate('local', function(err,user,info){
-		if (err || !user) {
-			return res.redirect('/login');
-		}
-		req.login(user,function(err){
-			return res.redirect('/');
-		})
-	})(req,res);
+module.exports.authenticate = function (req, res) {
+    console.log(req.body);
+    passportConfig.authenticate('local', function (err, user, info) {
+        if (err || !user) {
+            return res.redirect('/login');
+        }
+        req.login(user, function (err) {
+            return res.redirect('/');
+        })
+    })(req, res);
+};
+// EMPLOYEE AUTHENTICATION TABLE
+//module.exports.authenticate = function (req, res) {
+//    console.log(req.body);
+//    passportConfig.authenticate('local', function (err, employee, info) {
+//        if (err || !employee) {
+//            return res.redirect('/login');
+//        }
+//        req.login(user, function (err) {
+//            return res.redirect('/');
+//        })
+//    })(req, res);
+//};
+
+module.exports.signup = function (req, res) {
+    res.render('signup');
 };
 
-module.exports.signup = function(req,res) {
-	res.render('signup');
+module.exports.register = function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        models.User.create({
+                email: req.body.email
+                , password: hash
+            })
+            .then(function (user) {
+                req.login(user, function (err) {
+                    return res.redirect('/');
+                })
+            });
+    });
 };
 
-module.exports.register = function(req,res) {
-	bcrypt.hash(req.body.password, saltRounds, function(err,hash){
-		models.User.create({
-			email : req.body.email,
-			password : hash
-		})
-			.then(function(user){
-				req.login(user,function(err){
-					return res.redirect('/');
-				})
-			});
-	});
-};
-
-module.exports.logout = function(req,res) {
-	req.logout();
-	res.redirect('/login');
+module.exports.logout = function (req, res) {
+    req.logout();
+    res.redirect('/login');
 }
